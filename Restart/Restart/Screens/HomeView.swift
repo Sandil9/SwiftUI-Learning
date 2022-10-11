61222fe8c86cf6870b5c977669a9ca426a7935c3
Sandil Hussain <sundiljutt618@gmail.com>
2022-10-11 10:42:21 +0500

    Initial commit

---
+++ Restart/Restart/Screens/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Sandil Hussain on 11/10/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    var body: some View {
+        VStack(spacing: 20) {
+            // MARK: HEADER
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            // MARK: CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: FOOTER
+            Spacer()
+            
+            Button {
+                isOnboardingViewActive = true
+            } label: {
+                
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//: Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ SwiftUI-Tutorial/SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Sandil Hussain on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
